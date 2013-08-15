8b765905cdae00e1d71764e2b1bf89bce74d016d
RavWar <rav_war@mail.ru>
2013-08-15 22:58:34 +0400

    Remove default attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,3 @@
 default['logstash']['elasticsearch_ip'] = '33.33.33.10'
-default['logstash']['server']['enable_embedded_es'] = true
 default['logstash']['server']['xms'] = '256M'
 default['logstash']['server']['xmx'] = '256M'
