470cec421b2f4a34a4e59e63c8d9545af757ea28
RavWar <rav_war@mail.ru>
2013-09-14 16:29:38 +0400

    Increase memory pool for logstash

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
 default['logstash']['elasticsearch_ip'] = '33.33.33.10'
-default['logstash']['server']['xms'] = '256M'
-default['logstash']['server']['xmx'] = '256M'
+default['logstash']['server']['xms'] = '1024M'
+default['logstash']['server']['xmx'] = '1024M'
